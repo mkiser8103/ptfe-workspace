2b25fae0e6311f50e6a2808793e27018f56e9181
Mark Kiser <mkiser@hashicorp.com>
2019-08-22 13:53:52 -0500

    Updated main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ variable "vm_count" {
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-tfe-rg-${count.index}"
   location = "${var.location}"
-  count    = var.vm_count
+  count    = "${var.vm_count}"
   tags = {
     "environment" = "${var.environment}"
     "owner" = "${var.owner}"
